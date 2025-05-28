49fd78eba6b7dc749c7c04e5c1f4360d5c212c79
ihoek <ihoek679@naver.com>
2025-05-29 02:23:55 +0900

    fix : 피보호자 -> 보호자

--- neul-manager/src/features/Paylist/index.tsx
+++ neul-manager/src/features/Paylist/index.tsx
@@ -36,7 +36,7 @@ const Paylist = () => {
       key: "orderId",
     },
     {
-      title: "피보호자",
+      title: "보호자",
       dataIndex: "userName",
       key: "userName",
     },
@@ -67,7 +67,7 @@ const Paylist = () => {
           "/program/payment-list",
           { params: { type: "user" } }
         );
-        //console.log("Da", res.data);
+        console.log("Da", res.data);
 
         const resdata = res.data;
         const filterdata = resdata.filter(
